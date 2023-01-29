4094e020bb54b23e2eaf755e5471a27de42c4e73
LauraK0 <laurakeating@live.co.uk>
2023-01-29 12:13:07 +0000

    test 5 passes

--- src/routes/confessions.js
+++ src/routes/confessions.js
@@ -2,19 +2,18 @@ const {
   listConfessions,
   createConfession,
 } = require("../model/confessions.js");
-// const { getSession } = require("../model/sessions.js");
+const { getSession } = require("../model/session.js");
 const { Layout } = require("../templates.js");
 
 function get(req, res) {
-  /**
-   * Currently any user can view any other user's private confessions!
-   * We need to ensure only the logged in user can see their page.
-   * [1] Get the session ID from the cookie
-   * [2] Get the session from the DB
-   * [3] Get the logged in user's ID from the session
-   * [4] Get the page owner from the URL params
-   * [5] If the logged in user is not the page owner send a 401 response
-   */
+  const sid = req.signedCookies.sid; // * [1] Get the session ID from the cookie
+  const session = getSession(sid); // * [2] Get the session from the DB
+  const current_user = session && session.user_id; // * [3] Get the logged in user's ID from the session
+  const page_owner = Number(req.params.user_id); // * [4] Get the page owner from the URL params
+  if (current_user !== page_owner) {
+    // * [5] If the logged in user is not the page owner send a 401 response
+    return res.status(401).send("You are not authorized to view this page");
+  }
   const confessions = listConfessions(req.params.user_id);
   const title = "Your secrets";
   const content = /*html*/ `
