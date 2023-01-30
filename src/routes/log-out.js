13e70da83bd30314571bfdbdbd54eadf02974712
LauraK0 <laurakeating@live.co.uk>
2023-01-30 10:15:16 +0000

    get moved from log-out to home

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,24 +1,4 @@
-const { getSession } = require("../model/session");
 const { removeSession } = require("../model/session");
-const { Layout } = require("../templates.js");
-
-function get(req, res) {
-  const sid = req.signedCookies.sid;
-  const session = getSession(sid);
-  const title = "Confess your secrets!";
-  const content = /*html*/ `
-    <div class="Cover">
-      <h1>${title}</h1>
-      ${
-        session
-          ? /*html*/ `<form method="POST" action="/log-out"><button class="Button">Log out</button></form>`
-          : /*html*/ `<nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>`
-      }
-    </div>
-  `;
-  const body = Layout({ title, content });
-  res.send(body);
-}
 
 function post(req, res) {
   const sid = req.signedCookies.sid;
@@ -26,4 +6,4 @@ function post(req, res) {
   res.clearCookie("sid");
   res.redirect("/");
 }
-module.exports = { get, post };
+module.exports = { post };
